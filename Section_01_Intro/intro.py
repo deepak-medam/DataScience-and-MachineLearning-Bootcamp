da90212eeef8df2ed3612100c031c96f137cb551
deepak-medam <109440747+deepak-medam@users.noreply.github.com>
2023-10-04 05:15:05 +0100

    Section 01 files

---
+++ Section_01_Intro/intro.py
@@ -0,0 +1,17 @@
+"""
+* super vised learning: by providing labelled training data we can slowly teach our computer to do what you want.
+* At it's heart Data Science is about turning data into value.
+
+** Paper: From Data Mining to Knowledge Discovery in Data Bases by Usama Fayyad, Gregory Piatetsky-Shapiro, and Padhraic Smyth
+
+* Data mining is the application of specific algorithms for extracting patterns from data.
+* Data mining + Computer Science = Data Science
+* Why you need so much data.? Business Intelligence.
+* Data Science: almost everything that has something to do with data collecing, analyzing, modeling yet the most important part
+ is its applications.
+
+ ** Read about Maslow's hierarchy of needs.
+
+ * Data Engineer: Collect and store data. Data Scientist: Clean, explore, label, visualize the data and apply intelligent algorithms to
+  answer the questions with the data.
+"""
